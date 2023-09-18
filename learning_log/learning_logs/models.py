35c9820273ec35fad4fecc0034c3a7831c961b40
Kaion Murilo <116471682+Kaion-Murilo@users.noreply.github.com>
2023-09-18 09:02:21 -0300

    aula2

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    """Um assunto sobre o quaal o usuario esta aprendendo"""
+    text=models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devolve uma reprensenstacao em string do modelo"""
+        return self.text
